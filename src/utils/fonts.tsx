4983d549bb511d9dd5ceea6e56c023764e8e2197
rakibjrb <rakibjrb572157@gmail.com>
2025-02-18 00:46:50 +0600

    fixed google font subsets issue

--- src/utils/fonts.tsx
+++ src/utils/fonts.tsx
@@ -2,10 +2,12 @@ import { Acme, Karla } from "next/font/google";
 
 const acme = Acme({
   weight: "400",
+  subsets: ["latin"],
 });
 
 const karla = Karla({
   weight: ["300", "400", "600", "700", "800"],
+  subsets: ["latin"],
 });
 
 const font = {
