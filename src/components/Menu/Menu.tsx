f3e444072afafb2610f8e48350b104f7f5db7f7f
rakibjrb <rakibjrb572157@gmail.com>
2025-04-14 14:45:38 +0600

    updated menu bar and mission section

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import font from "@/utils/fonts";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaSearch } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -28,49 +28,79 @@ export default function Menu() {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 2xl:px-0 py-4 2xl:py-6 flex justify-between items-center relative">
-      <div className="flex gap-2 items-center">
-        <div className="w-10 h-10 bg-[#FF6969] rounded-lg"></div>
-        <span className={`${font.acme.className} text-3xl`}>Blood Bazar</span>
-      </div>
+    <div className="bg-white">
+      <div className="container mx-auto px-4 2xl:px-0 py-4 2xl:py-6 flex justify-between items-center relative">
+        <div className="flex gap-2 items-center">
+          <div className="w-10 h-10 bg-[#FF6969] rounded-lg"></div>
+          <span className={`${font.acme.className} text-3xl`}>Blood Bazar</span>
+        </div>
 
-      <ul className="hidden gap-5 lg:flex">
-        {links.map((page) => (
-          <li
-            key={page.link}
-            className={`${font.karla.className} ${
-              path === page.link ? "text-[#ff6969]" : ""
-            } text-xl hover:text-[#ff6969] transition-all duration-300 uppercase`}
-          >
-            <Link href={`${page.link}`}>{page.name}</Link>
+        <ul className="hidden gap-5 lg:flex lg:items-center">
+          {links.map((page) => (
+            <li
+              key={page.link}
+              className={`${font.karla.className} ${
+                path === page.link ? "text-[#ff6969]" : ""
+              } text-xl hover:text-[#ff6969] transition-all duration-300 uppercase`}
+            >
+              <Link href={`${page.link}`}>{page.name}</Link>
+            </li>
+          ))}
+
+          <li>
+            <Link
+              href={"/search"}
+              className="text-xl hover:text-[#ff6969] transition-all duration-300 flex items-center gap-3"
+            >
+              <FaSearch />
+              <div className="w-[100px] h-8 bg-[#F3F3F3] rounded-lg flex items-center lg:pl-4">
+                <span className="text-xl">Search</span>
+              </div>
+            </Link>
           </li>
-        ))}
-      </ul>
+        </ul>
 
-      <div className="lg:hidden">
-        <button onClick={mobile_menu_handler} className="text-2xl">
-          <FaBars />
-        </button>
-        <div
-          className={`fixed top-0 right-0 w-2/3 md:w-1/3 min-h-screen bg-[#ff9494] ${
-            show ? "" : "hidden"
-          } lg:hidden`}
-        >
-          <button onClick={mobile_menu_handler} className="text-2xl m-4">
-            <ImCross />
+        <div className="lg:hidden">
+          <button onClick={mobile_menu_handler} className="text-2xl">
+            <FaBars />
           </button>
-          <ul>
-            {links.map((page) => (
+          <div
+            className={`fixed top-0 right-0 w-2/3 md:w-1/3 min-h-screen bg-[#ff9494] ${
+              show ? "" : "hidden"
+            } lg:hidden`}
+          >
+            <button onClick={mobile_menu_handler} className="text-2xl m-4">
+              <ImCross />
+            </button>
+            <ul>
+              {links.map((page) => (
+                <li
+                  key={page.link}
+                  className={`${font.karla.className} ${
+                    path === page.link ? "bg-black" : ""
+                  } text-xl transition-all duration-300 uppercase text-white p-4 hover:bg-black`}
+                >
+                  <Link href={`${page.link}`}>{page.name}</Link>
+                </li>
+              ))}
+
               <li
-                key={page.link}
                 className={`${font.karla.className} ${
-                  path === page.link ? "bg-black" : ""
+                  path === "/search" ? "bg-black" : ""
                 } text-xl transition-all duration-300 uppercase text-white p-4 hover:bg-black`}
               >
-                <Link href={`${page.link}`}>{page.name}</Link>
+                <Link
+                  href={"/search"}
+                  className="text-xl hover:text-[#ff6969] transition-all duration-300 flex items-center gap-3"
+                >
+                  <FaSearch />
+                  <div className="w-[100px] h-8 bg-transparent rounded-lg flex items-center">
+                    <span className="text-xl">Search</span>
+                  </div>
+                </Link>
               </li>
-            ))}
-          </ul>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
